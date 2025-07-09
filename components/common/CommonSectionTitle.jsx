ab36878face124ca0dee91de648b60e990fce315
zobayerHosen <zobayerhosen2345@gmail.com>
2025-07-09 17:08:12 +0600

    new changes

---
+++ components/common/CommonSectionTitle.jsx
@@ -0,0 +1,12 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+const CommonSectionTitle = () => {
+    return (
+        <h3 className={cn("text-4xl font-semibold text-center text-primary-dark")}>
+            
+        </h3>
+    );
+};
+
+export default CommonSectionTitle;
