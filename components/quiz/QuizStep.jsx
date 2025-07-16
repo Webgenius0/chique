f8aee28fc7c5f179ff760a6dc488bc39fdb521dc
Farok-Hossain <farokafs7@gmail.com>
2025-07-16 09:47:16 +0600

    Quiz responsive updated

--- components/quiz/QuizStep.jsx
+++ components/quiz/QuizStep.jsx
@@ -2,7 +2,7 @@ const QuizStep = ({ number, question, options, onSelect, selectedOption }) => {
   return (
     <div className="w-full space-y-4 md:space-y-6">
       {/* Question Heading - Responsive text sizes and alignment */}
-      <h2 className="text-lg sm:text-xl md:text-2xl text-primary-dark font-semibold font-secondary text-start">
+      <h2 className=" md:text-2xl text-xl text-primary-dark font-semibold font-secondary text-start">
         {number}. {question}
       </h2>
 
@@ -11,7 +11,7 @@ const QuizStep = ({ number, question, options, onSelect, selectedOption }) => {
         {options.map((opt, idx) => (
           <label
             key={idx}
-            className={`w-full flex items-start md:items-center gap-2 sm:gap-3 md:gap-4 px-3 py-2 sm:px-4 sm:py-3 md:px-6 md:py-4 border rounded-lg cursor-pointer transition-all text-sm sm:text-base md:text-lg
+            className={`w-full flex justify-center items-center gap-3 lg:gap-4 lg:px-6 px-4 lg:py-4 py-3 border rounded-lg cursor-pointer transition-all text-base sm:text-lg
               ${selectedOption === opt
                 ? "bg-primary-light/10 border-primary-dark"
                 : "bg-white border-gray-200 hover:border-primary-dark/50"
@@ -25,9 +25,9 @@ const QuizStep = ({ number, question, options, onSelect, selectedOption }) => {
               checked={selectedOption === opt}
               onChange={() => onSelect(opt)}
               className="
-                w-4 h-4 sm:w-5 sm:h-5 
+                 lg:w-5 w-4 lg:h-5 h-5 
                 rounded-full border-gray-400 
-                text-primary-dark focus:ring-primary-dark bg-primary-dark
+                !text-primary-dark !focus:ring-primary-dark !bg-primary-dark
                 shrink-0 mt-0.5 sm:mt-0
               "
             />
