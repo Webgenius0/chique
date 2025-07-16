e42cd35945cd1ac6e8638c5f181443050b0fe286
Farok-Hossain <farokafs7@gmail.com>
2025-07-16 18:02:09 +0600

    responsive completed

--- app/dashboard/my-clothes/add-item/page.js
+++ app/dashboard/my-clothes/add-item/page.js
@@ -9,8 +9,8 @@ export const metadata = {
 
 const AddItem = () => {
   return (
-    <div className="w-full flex flex-col gap-6 pt-3">
-      <p className="text-2xl text-primary-dark font-special font-medium">
+    <div className="w-full flex flex-col gap-6 xs:py-10 py-5">
+      <p className="xs:text-2xl text-xl text-primary-dark font-special font-medium">
         Al Analysis Results
       </p>
       <AnalysisForm />
