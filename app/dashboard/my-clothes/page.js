3306716b0b3e37ae11f89dc5154b524fef9db577
Farok-Hossain <farokafs7@gmail.com>
2025-07-16 17:24:23 +0600

    MyClothe page responsive

--- app/dashboard/my-clothes/page.js
+++ app/dashboard/my-clothes/page.js
@@ -10,10 +10,10 @@ export const metadata = {
 };
 const MyClothes = () => {
   return (
-    <div className="w-full flex flex-col gap-10 pt-3">
-      <div className="w-full justify-between items-start flex gap-10">
+    <div className="w-full flex flex-col gap-10 xs:py-10 py-5">
+      <div className="w-full flex lg:flex-row flex-col justify-between items-start 2xl:gap-10 lg:gap-6 gap-10">
         <Wardrobe />
-        <div className="w-[700px] gap-20 flex flex-col justify-start items-start shrink-0">
+        <div className="2xl:w-[700px] lg:w-[450px] w-full flex flex-col sm:gap-20 gap-10 justify-start items-start shrink-0">
           <TodayPicks />
           <AIStylist />
         </div>
--- components/dashboard/clothe/Wardrobe.jsx
+++ components/dashboard/clothe/Wardrobe.jsx
@@ -8,30 +8,30 @@ import { GoFileDirectory } from "react-icons/go";
 const Wardrobe = () => {
 
   return (
-    <div className="w-full flex flex-col gap-6">
-      <p className="text-2xl text-primary-dark font-medium font-primary">
+    <div className="w-full flex flex-col xs:gap-6 gap-4">
+      <p className="xs:text-2xl text-xl text-primary-dark font-medium font-primary">
         Add New Item to Your Wardrobe
       </p>
-      <div className="w-full grid 2xl:grid-cols-3 xl:grid-cols-2 lg:grid-cols-3 sm:grid-cols-2 gap-4">
+      <div className="w-full grid 2xl:grid-cols-3 grid-cols-2  gap-4">
         {wardrobeItems.map((item) => (
           <Link
             className="w-full flex flex-col gap-3 cursor-pointer "
             key={item.id}
             href={`/dashboard/my-clothes/${item.category}`}
           >
-            <div className="w-full h-[290px] overflow-hidden border border-primary-dark rounded-2xl cursor-pointer">
+            <div className="w-full sm:h-[290px] h-[200px] overflow-hidden border border-primary-dark rounded-2xl cursor-pointer p-1.5">
               <Image
                 src={item.image}
                 className="w-full h-full object-cover"
                 alt={item.name}
               />
             </div>
-            <div className="w-full flex gap-4 bg-[#F8F8F8] p-4 rounded-lg">
-              <div className="w-12 h-12 bg-[#EDEDED] rounded-lg p-3">
-                <GoFileDirectory className="text-2xl" />
+            <div className="w-full flex sm:flex-row flex-col sm:gap-4 gap-2 bg-[#F8F8F8] sm:p-4 p-2 rounded-lg">
+              <div className="sm:w-12 w-8 sm:h-12 h-8 bg-[#EDEDED] rounded-lg sm:p-3 p-1.5">
+                <GoFileDirectory className="sm:text-2xl text-xl" />
               </div>
               <div className="flex flex-col gap-1">
-                <p className="text-base text-primary-dark font-primary">
+                <p className="3xs:text-base text-sm text-primary-dark font-primary">
                   {item.name}
                 </p>
                 <p className="text-sm text-primary-dark font-primary">
