93593403cd2f92c8b7567850678d1c247b6a015d
mafuj158 <mafujahmedbishal158@gmail.com>
2025-08-15 11:22:27 +0600

    new

--- middleware.js
+++ middleware.js
@@ -1,24 +1,25 @@
-// middleware.js
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-    const path = request.nextUrl.pathname
-    const isPublicPath = path === '/auth/sign-in' || path === '/auth/sign-up'
-    // Get token name from environment variable with fallback
-    const tokenName = process.env.AUTH_TOKEN_NAME || ''
-    const token = request.cookies.get(tokenName)?.value
-    // Redirect logic
+    const path = request.nextUrl.pathname;
+    const isPublicPath = path === '/auth/sign-in' || path === '/auth/sign-up';
+
+    const tokenName = process.env.AUTH_TOKEN_NAME || 'chique_auth_token';
+    const token = request.cookies.get(tokenName)?.value;
+    console.log("Token", token);
+
     if (!token && path.startsWith('/dashboard')) {
-        return NextResponse.redirect(new URL('/auth/sign-in', request.nextUrl))
+        return NextResponse.redirect(new URL('/auth/sign-in', request.url));
     }
     if (token && isPublicPath) {
-        return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
+        return NextResponse.redirect(new URL('/dashboard', request.url));
     }
 }
+
 export const config = {
     matcher: [
         '/dashboard/:path*',
         '/auth/sign-in',
-        '/auth/sign-up'
-    ]
-}
+        '/auth/sign-up',
+    ],
+};
