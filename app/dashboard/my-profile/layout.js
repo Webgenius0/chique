149a6993339f64a6882649cb1e1380e2f7f5eedd
mafuj158 <mafujahmedbishal158@gmail.com>
2025-09-04 17:07:11 +0600

    new changes

---
+++ app/dashboard/my-profile/layout.js
@@ -0,0 +1,40 @@
+import NavLink from "@/components/common/NavLink";
+import PageWrapper from "@/components/dashboard/PageWrapper"
+
+const MyProfileLayout = ({ children }) => {
+    const navItems = [
+        {
+            path: "/dashboard/my-profile",
+            name: "Profile",
+            end: true,
+        },
+        {
+            path: "/dashboard/my-profile/subscription",
+            name: "Subscriptions",
+            end: true,
+        },
+    ];
+    // main render
+    return (
+        <PageWrapper>
+            <div className="flex gap-3 w-full border-b">
+                {navItems.map((item) => (
+                    <NavLink
+                        key={item.path}
+                        href={item.path}
+                        end={item.end}
+                        className="p-2 flex items-center gap-2  text-xl"
+                        activeClassName="bg-primary-dark text-white"
+                        inactiveClassName="text-primary-dark hover:bg-primary-dark hover:text-white"
+                    >
+                        <span className="text-2xl shrink-0">{item.icon}</span>
+                        <span>{item.name}</span>
+                    </NavLink>
+                ))}
+            </div>
+            {children}
+        </PageWrapper>
+    )
+}
+
+export default MyProfileLayout
---
+++ app/dashboard/my-profile/subscription/page.js
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: "Chique | My Subscription",
+    description:
+        "Securely access your Chique account to manage your style profile, wardrobe, and personalized recommendations.",
+};
+const Subscription = () => {
+    return (
+        <div>page</div>
+    )
+}
+
+export default Subscription
